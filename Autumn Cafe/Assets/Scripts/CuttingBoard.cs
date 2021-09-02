d661c8c1e719da58e7a299d7edc6dbdf21cf2cd7
AvatarMars <markagianturco@gmail.com>
2021-09-02 18:41:46 -0400

    Basic interaction and pickup items

---
+++ Autumn Cafe/Assets/Scripts/CuttingBoard.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:294ff26ce57fe28cd6fd17854e808cc3d38250491c6b29be70361625f8e15330
+size 215
---
+++ Autumn Cafe/Assets/Scripts/Interactible.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3a9c2c6d64c7df7302489ad8cee062c4750df9009d7725603a6ea305c4c25f79
+size 227
