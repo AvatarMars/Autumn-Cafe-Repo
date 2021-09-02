c9d69e41bbb083239256c953b58848368f41cfe2
AvatarMars <markagianturco@gmail.com>
2021-09-02 18:41:46 -0400

    Basic interaction and pickup items

--- Autumn Cafe/Assets/Scripts/Interactible.cs
+++ Autumn Cafe/Assets/Scripts/Interactible.cs
@@ -1,13 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class Interactible : MonoBehaviour
-{
-    public bool interactableWithHeldItem;
-    public GameObject player;
-    public string tooltipText;
-
-    public abstract void interactFunction();
-   
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:d20c0273187071c0d0cfa3ccad8d493253cd4c028b0b78fe4a9550ec7874a8ff
+size 491
