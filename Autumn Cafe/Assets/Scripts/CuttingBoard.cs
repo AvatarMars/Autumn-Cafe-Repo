f8d33b32afc11948ced4520a124655c9c554e066
AvatarMars <markagianturco@gmail.com>
2021-09-02 18:41:46 -0400

    Basic interaction and pickup items

---
+++ Autumn Cafe/Assets/Scripts/CuttingBoard.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CuttingBoard : Interactible
+{
+    public override void interactFunction()
+    {
+        Debug.Log("interacting");
+    }
+}
---
+++ Autumn Cafe/Assets/Scripts/Interactible.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Interactible : MonoBehaviour
+{
+    public bool interactableWithHeldItem;
+
+    public abstract void interactFunction();
+   
+}
