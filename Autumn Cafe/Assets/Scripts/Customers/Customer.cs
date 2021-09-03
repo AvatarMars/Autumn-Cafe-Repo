9e891ce52682e48d5ca536c8cf8d062721256911
Ariel Fermin <afermin266@gmail.com>
2021-09-02 22:13:40 -0400

    Add Timer (patience for customers)

--- Autumn Cafe/Assets/Scripts/Customers/Customer.cs
+++ Autumn Cafe/Assets/Scripts/Customers/Customer.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5283206e02b52f60fd40f6ff9491026cfd73b4b9dabfba93215cf6824954ac42
-size 274
+oid sha256:62ec794eee56608efd723e15c3c49f326da8bfbe74d1a8ef68557cb419b93dc6
+size 288
