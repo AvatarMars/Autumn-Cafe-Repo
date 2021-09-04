5748d0dbb4f7612e6c7262c57137853184f41e4d
Ariel Fermin <afermin266@gmail.com>
2021-09-04 17:42:22 -0400

    Clean SampleScene

--- Autumn Cafe/Assets/Scripts/Initializer.cs
+++ Autumn Cafe/Assets/Scripts/Initializer.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dfc4a244f31f2b1ee4db24cc1bc6327f81f68303c9f15b1faac9d824fc1092a1
-size 1084
+oid sha256:9097187cbeb7a521fef779631b25197b256ed1b697051dd159c29104272bacca
+size 1148
