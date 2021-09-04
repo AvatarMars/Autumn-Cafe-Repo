0edf5fde901803f3611652fab7e3288636bcdeaa
AvatarMars <markagianturco@gmail.com>
2021-09-04 00:20:20 -0400

    oven

--- Autumn Cafe/Assets/Scripts/Interactible.cs
+++ Autumn Cafe/Assets/Scripts/Interactible.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:307f498e5c4e87729448d0d4591bc1be33aeaf3b05ae22323dabfc787d985886
-size 257
+oid sha256:e2000b047b84f9efd08601d897fbc38c201572efe26dafb77b9b44e33c7097f0
+size 288
