c9a2b3e43c85a4be1e95fece6df74b0f1e503d0f
AvatarMars <markagianturco@gmail.com>
2021-09-07 16:34:38 -0400

    base system in place

---
+++ Autumn Cafe/Assets/Scripts/CharacterScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterScript : MonoBehaviour
+{
+    public string characterName;
+    public Animator anim;
+    public Image img;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void PlayAnimation(string _name)
+    {
+        switch (_name)
+        {
+            case "neutral":
+                anim.SetTrigger("toNeutral");
+                break;
+            case "smile":
+                anim.SetTrigger("toSmile");
+                break;
+            case "frown":
+                anim.SetTrigger("toFrown");
+                break;
+        }
+    }
+
+
+}
--- Autumn Cafe/Assets/Scripts/Managers/DialogueManager.cs
+++ Autumn Cafe/Assets/Scripts/Managers/DialogueManager.cs
@@ -1,176 +1,219 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-//using UnityEngine.UI;
-//using Ink.Runtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Ink.Runtime;
+using TMPro;
 
-//public class DialogueManager : MonoBehaviour
-//{
-//    public TextAsset inkFile;
-//    public GameObject textBox;
-//    public GameObject customButton;
-//    public GameObject optionPanel;
-//    public bool isTalking = false;
+public class DialogueManager : MonoBehaviour
+{
+    public TextAsset inkFile;
+    public GameObject textBox;
+    public GameObject customButton;
+    public GameObject optionPanel;
+    public Transform[] optionPositions;
+    public bool isTalking = false;
 
-//    static Story story;
-//    Text nametag;
-//    Text message;
-//    List<string> tags;
-//    static Choice choiceSelected;
+    static Story story;
+    public TMP_Text nametag;
+    public TMP_Text message;
+    List<string> tags;
+    static Choice choiceSelected;
 
-//    // Start is called before the first frame update
-//    void Start()
-//    {
-//        story = new Story(inkFile.text);
-//        nametag = textBox.transform.GetChild(0).GetComponent<Text>();
-//        message = textBox.transform.GetChild(1).GetComponent<Text>();
-//        tags = new List<string>();
-//        choiceSelected = null;
-//    }
+    public CharacterScript activeCharacter;
+    public CharacterScript[] allCharacters;
+    public Transform[] characterPositions;
+    // Start is called before the first frame update
+    void Start()
+    {
+        story = new Story(inkFile.text);
+        tags = new List<string>();
+        choiceSelected = null;
+    }
 
-//    private void Update()
-//    {
-//        if (Input.GetKeyDown(KeyCode.Space))
-//        {
-//            //Is there more to the story?
-//            if (story.canContinue)
-//            {
-//                nametag.text = "Phoenix";
-//                AdvanceDialogue();
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Is there more to the story?
+            if (story.canContinue)
+            {
+                if (activeCharacter)
+                {
+                    nametag.text = activeCharacter.characterName;
+                    activeCharacter.img.color = new Color32(255,255,255, 255);
+                }
+                else
+                {
+                    nametag.text = "";
+                    foreach(CharacterScript chr in allCharacters)
+                    {
+                        chr.img.color = new Color32(100, 100, 100, 255);
+                    }
+                }
 
-//                //Are there any choices?
-//                if (story.currentChoices.Count != 0)
-//                {
-//                    StartCoroutine(ShowChoices());
-//                }
-//            }
-//            else
-//            {
-//                FinishDialogue();
-//            }
-//        }
-//    }
+                AdvanceDialogue();
 
-//    // Finished the Story (Dialogue)
-//    private void FinishDialogue()
-//    {
-//        Debug.Log("End of Dialogue!");
-//    }
+                //Are there any choices?
+                if (story.currentChoices.Count != 0)
+                {
+                    StartCoroutine(ShowChoices());
+                }
+            }
+            else
+            {
+                FinishDialogue();
+            }
+        }
+    }
 
-//    // Advance through the story 
-//    void AdvanceDialogue()
-//    {
-//        string currentSentence = story.Continue();
-//        ParseTags();
-//        StopAllCoroutines();
-//        StartCoroutine(TypeSentence(currentSentence));
-//    }
+    // Finished the Story (Dialogue)
+    private void FinishDialogue()
+    {
+        Debug.Log("End of Dialogue!");
+    }
 
-//    // Type out the sentence letter by letter and make character idle if they were talking
-//    IEnumerator TypeSentence(string sentence)
-//    {
-//        message.text = "";
-//        foreach (char letter in sentence.ToCharArray())
-//        {
-//            message.text += letter;
-//            yield return null;
-//        }
-//        CharacterScript tempSpeaker = GameObject.FindObjectOfType<CharacterScript>();
-//        if (tempSpeaker.isTalking)
-//        {
-//            SetAnimation("idle");
-//        }
-//        yield return null;
-//    }
+    // Advance through the story 
+    void AdvanceDialogue()
+    {
+        string currentSentence = story.Continue();
+        ParseTags();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(currentSentence));
+    }
 
-//    // Create then show the choices on the screen until one got selected
-//    IEnumerator ShowChoices()
-//    {
-//        Debug.Log("There are choices need to be made here!");
-//        List<Choice> _choices = story.currentChoices;
+    // Type out the sentence letter by letter and make character idle if they were talking
+    IEnumerator TypeSentence(string sentence)
+    {
+        message.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            message.text += letter;
+            yield return null;
+        }
+        yield return null;
+    }
 
-//        for (int i = 0; i < _choices.Count; i++)
-//        {
-//            GameObject temp = Instantiate(customButton, optionPanel.transform);
-//            temp.transform.GetChild(0).GetComponent<Text>().text = _choices[i].text;
-//            temp.AddComponent<Selectable>();
-//            temp.GetComponent<Selectable>().element = _choices[i];
-//            temp.GetComponent<Button>().onClick.AddListener(() => { temp.GetComponent<Selectable>().Decide(); });
-//        }
+    // Create then show the choices on the screen until one got selected
+    IEnumerator ShowChoices()
+    {
+        Debug.Log("There are choices need to be made here!");
+        List<Choice> _choices = story.currentChoices;
 
-//        optionPanel.SetActive(true);
+        for (int i = 0; i < _choices.Count; i++)
+        {
+            GameObject temp = Instantiate(customButton, optionPositions[i]);
+            temp.transform.GetChild(0).GetComponent<TMP_Text>().text = _choices[i].text;
+            temp.AddComponent<Selectable>();
+            temp.GetComponent<Selectable>().element = _choices[i];
+            temp.GetComponent<Button>().onClick.AddListener(() => { temp.GetComponent<Selectable>().Decide(); });
+        }
 
-//        yield return new WaitUntil(() => { return choiceSelected != null; });
+        optionPanel.SetActive(true);
 
-//        AdvanceFromDecision();
-//    }
+        yield return new WaitUntil(() => { return choiceSelected != null; });
 
-//    // Tells the story which branch to go to
-//    public static void SetDecision(object element)
-//    {
-//        choiceSelected = (Choice)element;
-//        story.ChooseChoiceIndex(choiceSelected.index);
-//    }
+        AdvanceFromDecision();
+    }
 
-//    // After a choice was made, turn off the panel and advance from that choice
-//    void AdvanceFromDecision()
-//    {
-//        optionPanel.SetActive(false);
-//        for (int i = 0; i < optionPanel.transform.childCount; i++)
-//        {
-//            Destroy(optionPanel.transform.GetChild(i).gameObject);
-//        }
-//        choiceSelected = null; // Forgot to reset the choiceSelected. Otherwise, it would select an option without player intervention.
-//        AdvanceDialogue();
-//    }
+    // Tells the story which branch to go to
+    public static void SetDecision(object element)
+    {
+        choiceSelected = (Choice)element;
+        story.ChooseChoiceIndex(choiceSelected.index);
+    }
 
-//    /*** Tag Parser ***/
-//    /// In Inky, you can use tags which can be used to cue stuff in a game.
-//    /// This is just one way of doing it. Not the only method on how to trigger events. 
-//    void ParseTags()
-//    {
-//        tags = story.currentTags;
-//        foreach (string t in tags)
-//        {
-//            string prefix = t.Split(' ')[0];
-//            string param = t.Split(' ')[1];
+    // After a choice was made, turn off the panel and advance from that choice
+    void AdvanceFromDecision()
+    {
+        optionPanel.SetActive(false);
+        foreach (Transform option in optionPositions)
+        {
+            if (option.childCount >= 1)
+            {
+                Destroy(option.GetChild(0).gameObject);
+            }
+        }
+        choiceSelected = null; // Forgot to reset the choiceSelected. Otherwise, it would select an option without player intervention.
+        AdvanceDialogue();
+    }
 
-//            switch (prefix.ToLower())
-//            {
-//                case "anim":
-//                    SetAnimation(param);
-//                    break;
-//                case "color":
-//                    SetTextColor(param);
-//                    break;
-//            }
-//        }
-//    }
-//    void SetAnimation(string _name)
-//    {
-//        CharacterScript cs = GameObject.FindObjectOfType<CharacterScript>();
-//        cs.PlayAnimation(_name);
-//    }
-//    void SetTextColor(string _color)
-//    {
-//        switch (_color)
-//        {
-//            case "red":
-//                message.color = Color.red;
-//                break;
-//            case "blue":
-//                message.color = Color.cyan;
-//                break;
-//            case "green":
-//                message.color = Color.green;
-//                break;
-//            case "white":
-//                message.color = Color.white;
-//                break;
-//            default:
-//                Debug.Log($"{_color} is not available as a text color");
-//                break;
-//        }
-//    }
+    /*** Tag Parser ***/
+    /// In Inky, you can use tags which can be used to cue stuff in a game.
+    /// This is just one way of doing it. Not the only method on how to trigger events. 
+    void ParseTags()
+    {
+        tags = story.currentTags;
+        foreach (string t in tags)
+        {
+            string prefix = t.Split(' ')[0];
+            string param = t.Split(' ')[1];
+            string paramTwo = t.Split(' ')[2];
 
-//}
+            switch (prefix.ToLower())
+            {
+                case "anim":
+                    SetAnimation(param);
+                    break;
+                case "color":
+                    SetTextColor(param);
+                    break;
+                case "move":
+                    SetPosition(param, paramTwo);
+                    break;
+                case "flip":
+                    SetFlip(param, paramTwo);
+                    break;
+            }
+        }
+    }
+    void SetAnimation(string _name)
+    {
+        CharacterScript cs = GameObject.FindObjectOfType<CharacterScript>();
+        cs.PlayAnimation(_name);
+        Debug.Log("here we would play the animation");
+    }
+    void SetTextColor(string _color)
+    {
+        switch (_color)
+        {
+            case "red":
+                message.color = Color.red;
+                break;
+            case "blue":
+                message.color = Color.cyan;
+                break;
+            case "green":
+                message.color = Color.green;
+                break;
+            case "white":
+                message.color = Color.white;
+                break;
+            default:
+                Debug.Log($"{_color} is not available as a text color");
+                break;
+        }
+    }
+
+    void SetPosition(string actor,string position)
+    {
+        foreach(CharacterScript chr in allCharacters)
+        {
+            if(chr.characterName == actor)
+            {
+                chr.transform.position = characterPositions[int.Parse(position)].position;
+            }
+        }
+    }
+
+    void SetFlip(string actor, string flipBool)
+    {
+        foreach (CharacterScript chr in allCharacters)
+        {
+            if (chr.characterName == actor)
+            {
+                chr.GetComponent<SpriteRenderer>().flipX = bool.Parse(flipBool);
+            }
+        }
+    }
+}
