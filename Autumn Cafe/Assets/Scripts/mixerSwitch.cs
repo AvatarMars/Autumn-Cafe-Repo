bf522699ec27cd29615a0c550fd939ab3a09a7ab
AvatarMars <markagianturco@gmail.com>
2021-09-10 16:59:06 -0400

    animated mixer

--- Autumn Cafe/Assets/Scripts/mixerSwitch.cs
+++ Autumn Cafe/Assets/Scripts/mixerSwitch.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:03682e1c836c2493985ada6e080b18bbed0d745e329e618f60f2b28f956153e9
-size 400
+oid sha256:582a8e164d61256a0846a72dc02a1f913628ebdc98022ffaecd939ccba295866
+size 437
